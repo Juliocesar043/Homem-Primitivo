1e96cbfa3c759cc8171497aa97ffcf17ae9940d2
educrispim <educrispim@github.com>
2026-08-19 16:10:01 -0300

    Adicionada mecanica de interagir

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,40 +5,50 @@ extends CharacterBody2D
 
 var was_in_air: bool = false
 var is_landing: bool = false
+var is_interacting: bool = false
+var estaMovendo: bool = false
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
 
+
+
 func _physics_process(delta: float) -> void:
-	# Aplica a gravidade
+	# Aplica a gravidade sempre
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
 
+	if Input.is_action_just_pressed("interagir") and is_on_floor() and not is_interacting:
+		is_interacting = true
+		
+		
 	# Lida com o pulo
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		somPulo.play()
 		
-
 	# Pega a direção e lida com o movimento
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	
 	move_and_slide()
 
-	# 1. Detecta o exato momento em que ele encosta no chão
+	# Detecta o exato momento em que ele encosta no chão
 	if is_on_floor() and was_in_air:
 		is_landing = true
 		anim.play("land")
 
-	# 2. Atualiza a variável para o próximo frame
+	# Atualiza a variável para o próximo frame
 	was_in_air = not is_on_floor()
 
-	# 3. Lógica de transição de animações
+	# Detecta quando está se movendo
+	if velocity.x != 0:
+		estaMovendo = true
+	
+	# Lógica de transição de animações padrão
 	if not is_on_floor():
 		if velocity.y < 0:
 			anim.play("jump") 
@@ -48,17 +58,20 @@ func _physics_process(delta: float) -> void:
 		if is_landing:
 			if velocity.x != 0:
 				is_landing = false
-				anim.play("walk") 
+				anim.play("walk")
+			elif Input.is_anything_pressed():
+					is_landing = false
+					anim.play("idle") 
 		else:
 			if direction > 0:
-				anim.flip_h = true 
+				anim.flip_h = true # Dica: Geralmente direita (>) é false 
 				anim.play("walk")
 			elif direction < 0:
-				anim.flip_h = false 
+				anim.flip_h = false  # Dica: Geralmente esquerda (<) é true
 				anim.play("walk")
-			else:
+			elif is_interacting == true:
+				anim.play("interagir")
+				await anim.animation_finished
+				is_interacting = false
+			elif not Input.is_anything_pressed() and not is_interacting:
 				anim.play("idle")
-
-func _on_animated_sprite_2d_animation_finished() -> void:
-	if anim.animation == "land":
-		is_landing = false
